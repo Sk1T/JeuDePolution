44b6ac5dac5a73c22704e0e725ab795198272734
KARELIN Artem <karelinskit@gmail.com>
2026-04-06 12:54:32 +0200

    Fixed all bugs and conflicts

--- scene/Level/level.gd
+++ scene/Level/level.gd
@@ -1,19 +1,17 @@
 # level.gd
 extends Node2D
-
+var rules_scene = preload("res://scene/pop_up_window.tscn")
 var level: int = 1
 
 func _ready():
 	show_rules()
-	
+	_setup_level(self)
 func show_rules():
 	var popup = rules_scene.instantiate()
 	var canvas = CanvasLayer.new()
 	add_child(canvas)
 	canvas.add_child(popup)
 	popup.setup("The goal of this game is to clean up the environment and pick up trash! You must pick up and recycle the trash.The game will end once you have recycled enough trash.", "START")
-func _ready() -> void:
-	_setup_level(self)
 
 func _setup_level(level_root: Node) -> void:
 	var player = level_root.get_node_or_null("Player")
